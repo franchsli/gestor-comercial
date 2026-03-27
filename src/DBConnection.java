113fab0e75b08a133eb89ed88bc9c8df3129d2d1
Francisco <112600427+franchsli@users.noreply.github.com>
2026-03-27 18:57:47 -0500

    feat: añade un método DBConnection para ejecutar SQL

--- src/DBConnection.java
+++ src/DBConnection.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DBConnection {
@@ -10,5 +11,15 @@ public class DBConnection {
             connection = DriverManager.getConnection("jdbc:sqlite:mi_base_de_datos.db");
         }
         return connection;
+    }
+
+    // Para INSERT/UPDATE/DELETE
+    protected void ejecutar(String sql) {
+        try (Connection conn = DBConnection.get();
+             PreparedStatement pStatement = conn.prepareStatement(sql)) {
+            pStatement.execute();
+        } catch (SQLException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
     }
 }
