f3c8d14fc59fc0666735c9434fe0d1d08a612a1d
Francisco <112600427+franchsli@users.noreply.github.com>
2026-03-30 15:42:10 -0500

    feat: añade 'S' al final del nombre de la clase ya que las tablas están en plural. También pone todo en mayúsculas (aunque no es necesario)

--- src/Modelo.java
+++ src/Modelo.java
@@ -14,7 +14,7 @@ import java.sql.PreparedStatement;
 
 public class Modelo {
     //protected String nombreTabla = "GASTOS";
-    protected String nombreTabla = this.getClass().getSimpleName();
+    protected String nombreTabla = this.getClass().getSimpleName().toUpperCase() + "S";
 
     // TODO: Añadir más métodos GENERALES
     // EJ: Borrar todos, borrar segun condición
