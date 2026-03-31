d055ada34be783d7e4b30876f25eb6ca7b215460
Francisco <112600427+franchsli@users.noreply.github.com>
2026-03-31 16:03:23 -0500

    fix: Añade espacio a las claúsulas WHERE

--- src/Modelo.java
+++ src/Modelo.java
@@ -28,7 +28,7 @@ public class Modelo {
     public List<Map<String, String>> todos(String condicion){
         String sql = "SELECT * FROM " + nombreTabla;
         if (!condicion.isEmpty()) {
-            sql += "WHERE " + condicion;
+            sql += " WHERE " + condicion;
         }
         List<Map<String, String>> resultados = new ArrayList<>();
         
@@ -68,7 +68,7 @@ public class Modelo {
     void borrarTodos(String condicion){
         String sql = "DELETE * FROM " + nombreTabla;
         if (!condicion.isEmpty()) {
-            sql += "WHERE " + condicion;
+            sql += " WHERE " + condicion;
         }
         DBConnection.ejecutar(sql);
     }
@@ -186,7 +186,6 @@ public class Modelo {
         // actualiza un registro (en este caso, el último)
         LinkedHashMap<String, String> nuevosDatos = new LinkedHashMap<>();
         nuevosDatos.put("estado", "PENDIENTE");
-        // TODO: BUG: No such column: PENDIENTE
         model.actualizar(nuevosDatos, "id=11");
         // verifica que el nuevo registro está actualizado
         resultados = model.todos("id=11");
