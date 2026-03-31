d0bf3b056d2042f0298000365358b87383b4169a
Francisco <112600427+franchsli@users.noreply.github.com>
2026-03-31 17:42:12 -0500

    docs: añade JavaDOC para los métodos añadidos

--- src/Gasto.java
+++ src/Gasto.java
@@ -14,24 +14,51 @@ public class Gasto extends Modelo {
         actualizar(gastoPagado, condicion);
     }
 
+    /**
+     * Devuelve todos los gastos pendientes en
+     * la base de datos.
+     * @return Los gastos pendientes en la base de datos.
+     */
     List<Map<String, String>> pendientes(){
         String condicion = "estado='PENDIENTE'";
         return todos(condicion);
     }
 
+    /**
+     * Devuelve todos los gastos pagados en
+     * la base de datos.
+     * @return Los gastos pagados en la base de datos.
+     */
     List<Map<String, String>> pagados(){
         String condicion = "estado='PAGADO'";
         return todos(condicion);
     }
 
+    /**
+     * Devuelve todos los gastos antes de la fecha dada.
+     * @param fecha La fecha en formato AAAA-MM-DD.
+     * @return Todos los gastos previos a esa fecha.
+     */
     List<Map<String, String>> todosAntes(String fecha){
         String condicion = "fecha<" + fecha;
         return todos(condicion);
     }
+
+    /**
+     * Devuelve todos los gastos después de la fecha dada.
+     * @param fecha La fecha en formato AAAA-MM-DD.
+     * @return Todos los gastos posteriores a esa fecha.
+     */
     List<Map<String, String>> todosDespues(String fecha){
         String condicion = "fecha>" + fecha;
         return todos(condicion);
     }
+
+    /**
+     * Devuelve todos los gastos en la fecha dada.
+     * @param fecha La fecha en formato AAAA-MM-DD.
+     * @return Todos los gastos previos en esa fecha.
+     */
     List<Map<String, String>> todosEn(String fecha){
         String condicion = "fecha=" + fecha;
         return todos(condicion);
