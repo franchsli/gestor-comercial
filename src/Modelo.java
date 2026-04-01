ce3d38914785df59c54d6725cb3adb5e525fcbac
Francisco <112600427+franchsli@users.noreply.github.com>
2026-03-31 19:16:59 -0500

    feat: insertar() recibe un array de Strings ahora para hacer más fácil el llamado

--- src/Modelo.java
+++ src/Modelo.java
@@ -75,11 +75,10 @@ public class Modelo {
     /**
      * Inserta los datos dados en la tabla.
      * @param datos Los datos a insertar. 
-     * Deben ir entre parentesis y separados por comas
-     * Ej: "(1,2,3,4,5,'texto')"
      */
-    void insertar(String datos){
-        String sql = "INSERT INTO " + nombreTabla + " VALUES " + datos;
+    void insertar(String[] datos){
+        String datosSql = formatearValores(datos);
+        String sql = "INSERT INTO " + nombreTabla + " VALUES " + datosSql;
         DBConnection.ejecutar(sql);
     }
 
@@ -193,7 +192,8 @@ public class Modelo {
         System.out.println(contadorCondicionado);
         System.out.println("----------");
         // inserta un nuevo registro
-        model.insertar("(null, '2026-02-25', 'PAGADO', 50000)");
+        String[] nuevoRegistro = {"2026-02-25", "PAGADO", "50000"};
+        model.insertar(nuevoRegistro);
         // verifica que esté el nuevo registro
         resultados = model.todos();
         for (Map<String,String> map : resultados) {
