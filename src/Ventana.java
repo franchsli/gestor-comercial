1444fa7f2b906967d4361feb29e5ea159f3ea1f6
Francisco <112600427+franchsli@users.noreply.github.com>
2026-04-14 16:26:52 -0500

    feat: crea la ventana principal. Closes #13

---
+++ src/Ventana.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+public class Ventana extends JFrame {
+    public Ventana(){
+        setTitle("Gestor Comercial");
+        setSize(800, 600);
+        // cierra el programa al cerrar la ventana
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // centra la ventana
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+}
