83c9cc98b4c19c3e300e1b53eace875dd930c83d
Francisco <112600427+franchsli@users.noreply.github.com>
2026-04-18 19:11:13 -0500

    feat: añade metodos para la clase Venta

--- src/Venta.java
+++ src/Venta.java
@@ -1,4 +1,8 @@
+import java.time.LocalDate;
+import java.util.Map;
+
 public class Venta extends Modelo {
+    Producto tablaProductos = new Producto();
     
     void actualizarFecha(String id, String nuevaFecha){
         actualizar("fecha", nuevaFecha, id);
@@ -10,5 +14,32 @@ public class Venta extends Modelo {
 
     void actualizarValor(String id, String nuevoValor){
         actualizar("valor", nuevoValor, id);
+    }
+
+
+    void crear(String fecha, String estado, String valor){
+        String[] datos = {fecha, estado, valor};
+        insertar(datos);
+    }
+
+    void crear(String estado, String valor){
+        LocalDate fechahoy = LocalDate.now();
+        String fechaHoyTexto = fechahoy.toString();
+        crear(fechaHoyTexto, estado, valor);
+    }
+
+    void crear(String valor){
+        crear("PENDIENTE", valor);
+    }
+
+    void crearVentaProducto(String ventaId, String productoId, String cantidadProducto){
+        String[] datos = {ventaId, productoId};
+        insertar(datos);
+    }
+
+    void vender(String ventaId, String nombreProducto, String cantidadProducto){
+        Map<String, String> producto = unicoRegistro("nombre", nombreProducto);
+        String productoId = producto.get("id");
+        crearVentaProducto(ventaId, productoId, cantidadProducto);
     }
 }
