247c62827646151fff4c45bf7d50d140fc0aa7d4
Francisco <112600427+franchsli@users.noreply.github.com>
2026-04-20 15:35:44 -0500

    feat: añade pestañas en blanco que representan las tablas principales en la BD. Closes #27

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,16 @@
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 public class Main {
     public static void main(String[] args) {
         Ventana ventana = new Ventana();
+        JTabbedPane pestañas = new JTabbedPane();
+        // se debe reemplazar JPanel con la implementación completa
+        pestañas.addTab("Productos", new JPanel());
+        pestañas.addTab("Ventas", new JPanel());
+        pestañas.addTab("Gastos", new JPanel());
+        //pestañas.addTab("Reportes", new JPanel());
+        ventana.add(pestañas);
         DBConnection.cerrar();
     }
 }
