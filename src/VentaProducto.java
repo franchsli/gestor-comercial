95a1e5223598b230286351f4c57474520e598c01
Francisco <112600427+franchsli@users.noreply.github.com>
2026-04-20 14:51:17 -0500

    fix: arregla la creación de ventas_productos no actualizando el inventario

--- src/VentaProducto.java
+++ src/VentaProducto.java
@@ -1,4 +1,5 @@
 public class VentaProducto extends Modelo {
+    Producto productos = new Producto();
 
     VentaProducto(){
         this.nombreTabla = "VENTAS_PRODUCTOS";
@@ -6,12 +7,15 @@ public class VentaProducto extends Modelo {
     }
 
     /**
-     * Crea un nuevo registro en la tabla VENTAS_PRODUCTOS.
+     * Crea un nuevo registro en la tabla VENTAS_PRODUCTOS y
+     * reduce el inventario de PRODUCTOS.
      * @param ventaId El id de la venta.
      * @param productoId El id del producto.
      * @param cantidadProducto La cantidad del producto en la venta.
      */
     void crear(String ventaId, String productoId, String cantidadProducto){
+        int cantidad = Integer.parseInt(cantidadProducto);
+        productos.reducirInventario(productoId, cantidad);
         String[] datos = {ventaId, productoId, cantidadProducto};
         insertar(datos);
     }
