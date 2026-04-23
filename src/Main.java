2f89c49e54c8240218d4d698f9e01e6be4c5707f
Francisco <112600427+franchsli@users.noreply.github.com>
2026-04-23 16:07:08 -0500

    feat: muestra la pestaña de productos (READ) (#29) en la ventana principal

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,14 @@ import javax.swing.JTabbedPane;
 
 public class Main {
     public static void main(String[] args) {
-        Ventana ventana = new Ventana();
         JTabbedPane pestañas = new JTabbedPane();
+        ProductosPanel pestañaProductos = new ProductosPanel();
         // se debe reemplazar JPanel con la implementación completa
-        pestañas.addTab("Productos", new JPanel());
+        pestañas.addTab("Productos", pestañaProductos);
         pestañas.addTab("Ventas", new JPanel());
         pestañas.addTab("Gastos", new JPanel());
         //pestañas.addTab("Reportes", new JPanel());
+        Ventana ventana = new Ventana();
         ventana.add(pestañas);
         DBConnection.cerrar();
     }
