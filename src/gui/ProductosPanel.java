3335c312f32f5b5a44f265bcdbc65583a4ddd0ef
Francisco <112600427+franchsli@users.noreply.github.com>
2026-04-26 18:55:37 -0500

    feat: implementa la creación y eliminación de productos

--- src/gui/ProductosPanel.java
+++ src/gui/ProductosPanel.java
@@ -1,12 +1,39 @@
 package gui;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import java.awt.GridLayout;
+
 import db.Producto;
 
 public class ProductosPanel extends Panel {
+    Producto productos = new Producto();
 
     public ProductosPanel() {
-        this.modelo = new Producto();
+        this.modelo = productos;
+        btnNuevo.addActionListener(e -> mostrarFormularioNuevo());
         cargarDatos();
     }
 
+    @Override
+    public void mostrarFormularioNuevo() {
+        JTextField nombre = new JTextField();
+        JTextField precio = new JTextField();
+        JTextField cantidad = new JTextField();
+
+        JPanel form = new JPanel(new GridLayout(3, 2, 5, 5));
+        form.add(new JLabel("Nombre:")); form.add(nombre);
+        form.add(new JLabel("Precio unitario:")); form.add(precio);
+        form.add(new JLabel("Cantidad:")); form.add(cantidad);
+
+        int result = JOptionPane.showConfirmDialog(this, form,
+            "Nuevo producto", JOptionPane.OK_CANCEL_OPTION);
+
+        if (result == JOptionPane.OK_OPTION) {
+            productos.crear(nombre.getText(), precio.getText(), cantidad.getText());
+            cargarDatos();
+        }
+    }
 
 }
