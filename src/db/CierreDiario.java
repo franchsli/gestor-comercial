3c6ffb0124d145cbcc7816dc7db7fe06eb49e54a
Francisco <112600427+franchsli@users.noreply.github.com>
2026-05-05 16:25:52 -0500

    fix: arregla el atributo nombreTabla teniendo el valor incorrecto en CierreDiario

--- src/db/CierreDiario.java
+++ src/db/CierreDiario.java
@@ -2,6 +2,7 @@ package db;
 
 public class CierreDiario extends Modelo {
     public CierreDiario(){
+        this.nombreTabla = "CIERRES_DIARIOS";
         this.columnas = "(fecha, ventas_efectivo, ventas_credito, total_ventas, presupuesto, cumplimiento, gastos)";
     }
 }
