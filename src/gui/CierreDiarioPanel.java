95dd27844cecb0e960a177209f551bba3887f80d
Francisco <112600427+franchsli@users.noreply.github.com>
2026-05-05 17:10:46 -0500

    feat: añade el metodo cargarTotales para mostrar la fila con los totales en la pestaña de cierres diarios

--- src/gui/CierreDiarioPanel.java
+++ src/gui/CierreDiarioPanel.java
@@ -15,4 +15,20 @@ public class CierreDiarioPanel extends Panel {
         this.campoBuscar.setVisible(false);
         cargarDatos();
     }
+
+    public void cargarTotales() {
+        String[] columnas = {"ventas_efectivo", "ventas_credito", "total_ventas", "presupuesto", "gastos"};
+        Object[] filaTotal = new Object[modeloTabla.getColumnCount()];
+        
+        filaTotal[0] = "TOTAL";
+        for (int i = 1; i < columnas.length + 1; i++) {
+            filaTotal[i] = cierresDiarios.sumarColumna(columnas[i - 1]);
+        }
+        // cumplimiento va en índice 5, se calcula aparte
+        int totalVentas = cierresDiarios.sumarColumna("total_ventas");
+        int totalPresupuesto = cierresDiarios.sumarColumna("presupuesto");
+        filaTotal[5] = totalPresupuesto == 0 ? "0%" : Math.round((totalVentas * 100.0) / totalPresupuesto) + "%";
+
+        modeloTabla.addRow(filaTotal);
+    }
 }
